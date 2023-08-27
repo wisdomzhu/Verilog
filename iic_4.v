dcf5f655142c4e9af8eac97304828f645109cc13
wisdomzhu <143255332+wisdomzhu@users.noreply.github.com>
2023-08-27 11:30:31 +0800

    Add files via upload

--- iic_4.v
+++ iic_4.v
@@ -1,49 +1,60 @@
 `timescale 1ns / 1ps
 module iic_master(
-  input clk_i,//150MHz
+  input sys_clk_p_i,
+  input sys_clk_n_i,
 
-  input [6:0] Slv_Addr_i,
-  input [7:0] Reg_Addr_i,
-  input [7:0] Data_i,
-
-  input wr_i,
-
-  output scl,//30KHz
-  /*inout*/output sda,
-  output [7:0]IIC_Read_Data,
+  output oled_scl_o,//30KHz
+  inout oled_sda_io,
   output oled_pclk_o,
   output oled_rst_o
-
 );
 
-parameter DELAY=10;//1500000;
-parameter DIV=4;//2500;//每DIV个clk_i的上升沿，翻转一次
+parameter DELAY=1500000;
+parameter DIV=2500;//每DIV个clk_i的上升沿，翻转一次
 parameter T1=DIV/4;
 parameter T2=DIV*3/4;
 
 reg [11:0] cnt_clk;						//clk计数器
 reg [3:0] cnt_scl;						//scl计数器，表示传送数据的位数
 reg sda_tri;							//三态控制信号,0:输出；1：输入
-reg sda_o;								//sda总线输出数据的寄存器
-//wire sda_i;                             //sda总线输入数据   
-reg scl_o;								//scl时钟总线
+reg sda_out_r/* synthesis mark_debug=true */;								//sda总线输出数据的寄存器
+wire sda_in_r/* synthesis mark_debug=true */;                             //sda总线输入数据   
+reg scl_r/* synthesis mark_debug=true */;								//scl时钟总线
 reg [4:0] current_state;				//当前状态
 reg [4:0] next_state;					//下个状态
-reg [7:0]IIC_Read_Data_o;
+reg [7:0] IIC_Read_Data_r;//读出来的数据放在寄存器里检验是否正确
 
 reg [4:0] current_state_1;
 reg [4:0] next_state_1;
 reg iic_send;
-reg rst_o;
+reg rst_r/* synthesis mark_debug=true */;//debug标记技巧
 reg [23:0]cnt_delay;
-reg busy=0;
+reg wr_flag;//0:写；1：读
 
-wire locked;
+//要输入的数据放在寄存器
+reg [6:0] Slv_Addr_i=7'b1010100;
+reg [7:0] Reg_Addr_i=8'b00000001;
+reg [7:0] Data_i=8'b00001011;
+
+//输入的差分时钟转单端,cmos_data_clk_150m_o作为全局时钟
+wire cmos_data_clk_150m;
+wire cmos_data_clk_150m_o;
+IBUFGDS
+CLK_U(
+	.I(sys_clk_p_i),
+	.IB(sys_clk_n_i),
+	.O(cmos_data_clk_150m)
+);
+BUFG
+BUFG_CLKIN1(
+    .I(cmos_data_clk_150m),
+	.O(cmos_data_clk_150m_o)
+);
 
 //内嵌系统复位
 wire rst_n;
 reg [7:0]cnt_rst=8'd0;
-always @(posedge clk_i) begin
+always @(posedge cmos_data_clk_150m_o) begin
 	if(cnt_rst==8'd150)
 	  cnt_rst<=cnt_rst;
 	else
@@ -52,11 +63,10 @@ end
 assign rst_n=(cnt_rst==150)?1:0;
 
 
-assign sda = sda_tri ? 1'bz : sda_o;
-//assign sda_i=sda;
-assign scl = scl_o;
-assign IIC_Read_Data=IIC_Read_Data_o;
-assign oled_rst_o=rst_o;
+assign oled_sda_io = sda_tri ? 1'bz : sda_out_r;
+assign sda_in_r=oled_sda_io;
+assign oled_scl_o = scl_r;
+assign oled_rst_o=rst_r;
 
 //states
 //states_1
@@ -83,8 +93,10 @@ localparam R_ACK3=5'd13;
 localparam R_DATA=5'd14;
 localparam R_ACK4=5'd15;
 
+
+
 //状态机1第一段
-always @(posedge clk_i) begin
+always @(posedge cmos_data_clk_150m_o) begin
 	if(~rst_n)
 		current_state_1<=PULL_UP;
     else
@@ -123,47 +135,56 @@ begin
   endcase
 end
 //状态机1第三段
-always@(posedge clk_i)
+always@(posedge cmos_data_clk_150m_o)
 begin
 	if(~rst_n)begin
-	   rst_o<=1;
+	   rst_r<=1;
 	   iic_send<=0;
 	   cnt_delay<=0; 
+	   wr_flag=0;
 	end
     else begin
   case(next_state_1) 
 	  PULL_UP:
 	         begin
-			  rst_o<=1;
+			  rst_r<=1;
 			  iic_send<=0;
 			  cnt_delay<=(cnt_delay>=DELAY-1)?0:cnt_delay+1; 
 			 end
 	  RST_L:
 	        begin
-			  rst_o<=0;
+			  rst_r<=0;
 			  iic_send<=0;
 			  cnt_delay<=(cnt_delay>=DELAY-1)?0:cnt_delay+1; 
 			end		
 	  RST_H:
 	        begin
-			  rst_o<=1;
+			  rst_r<=1;
 			  iic_send<=0;
 			  cnt_delay<=(cnt_delay>=DELAY-1)?0:cnt_delay+1; 
 			end	
 	  OVER:
 	       begin
-			  rst_o<=1;
+			  rst_r<=1;
 			  iic_send<=1;
 			  cnt_delay<=0;
-		   end			 
+			  wr_flag<=0;//前三个状态都对wr_flag没操作，故在前三个状态，wr_flag保持初值
+		   end	
+	  default://以上状态都跳不进去，则默认为OVER
+	  	   begin
+			  rst_r<=1;
+			  iic_send<=1;
+			  cnt_delay<=0;
+			  wr_flag<=0;
+		   end		 
   endcase
 	end
 end
 
 //状态机2第一段
-always@(posedge clk_i)
+always@(posedge cmos_data_clk_150m_o)
 begin 
-	if(~rst_o)
+	if(~rst_r)
 		current_state <= IDLE;
 	else
 		current_state <= next_state;
@@ -203,7 +224,7 @@ begin
 			end                     
 		W_ACK1:
 			begin
-				if(cnt_clk == 12'b0)
+				if((cnt_clk == 12'b0)&&(sda_in_r==0))
 					next_state <= W_REG_ADDR;
 				else
 					next_state <= current_state;
@@ -217,9 +238,9 @@ begin
 			end
 		W_ACK2:
 			begin
-				if((cnt_clk == 12'b0)&&(wr_i==0))
+				if((cnt_clk == 12'b0)&&(wr_flag==0)&&(sda_in_r==0))
 					next_state <= W_DATA;
-				else if ((cnt_clk == 12'b0)&&(wr_i==1))
+				else if ((cnt_clk == 12'b0)&&(wr_flag==1)&&(sda_in_r==0))
 				    next_state <= R_START2;
                      else
                 	next_state <= current_state;
@@ -233,25 +254,21 @@ begin
 			end
 		W_ACK3:
 			begin
-				if(cnt_clk == 12'b0)
+				if((cnt_clk == 12'b0)&&(sda_in_r==0))
 					next_state <= STOP;
 				else
 					next_state <= current_state;
 			end 
 		STOP:
 			begin
-				if((cnt_clk == 12'b0)&&(busy==1))
-					next_state <= IDLE;
-				else begin
-				  if((cnt_clk == 12'b0)&&(busy==0))
+				if(cnt_clk == 12'b0)
 					next_state <= FINISH;
-					else
+				else
 					next_state <= current_state;
-				end
 			end
 		FINISH:
 		    begin
-				next_state<=current_state;
+				next_state <= current_state;
 			end	
 		R_START2:
 			begin
@@ -276,7 +293,7 @@ begin
 			end    
 		R_ACK3:
 			begin
-				if(cnt_clk == 12'b0)
+				if((cnt_clk == 12'b0)&&(sda_in_r==0))
 					next_state <= R_DATA;
 				else
 					next_state <=current_state;
@@ -300,16 +317,16 @@ begin
 end
 
 //状态机2第三段
-always@(posedge clk_i)
+always@(posedge cmos_data_clk_150m_o)
 begin
-	if(~rst_o)
+	if(~rst_r)
 		begin
 			sda_tri 	<= 1'b0;
-			scl_o 		<= 1'b1;
-			sda_o 		<= 1'b1;
+			scl_r 		<= 1'b1;
+			sda_out_r 		<= 1'b1;
 			cnt_clk 	<= 12'b0;
 			cnt_scl 	<= 4'b0;
-            IIC_Read_Data_o <= 8'b0;
+            IIC_Read_Data_r <= 8'b0;
 		end
 	else 
 		begin
@@ -317,149 +334,169 @@ begin
 				IDLE:
 					begin
 						sda_tri 	<= 1'b0;
-						scl_o 		<= 1'b1;
-						sda_o 		<= 1'b1;
+						scl_r 		<= 1'b1;
+						sda_out_r 		<= 1'b1;
 						cnt_clk 	<= 12'b0;
 						cnt_scl 	<= 4'b0;
-                        IIC_Read_Data_o <= 8'b0;                        
+                        IIC_Read_Data_r <= 8'b0;                        
 					end   
 				W_START:
 					begin
 						sda_tri 	<= 1'b0;
-						scl_o 		<= (cnt_clk<T2) ? 1'b1 : 1'b0; 
-						sda_o 		<= (cnt_clk<T1) ? 1'b1 : 1'b0; 
+						scl_r 		<= (cnt_clk<T2) ? 1'b1 : 1'b0; 
+						sda_out_r 		<= (cnt_clk<T1) ? 1'b1 : 1'b0; 
 						cnt_clk 	<= (cnt_clk>=DIV-1) ? 0 : cnt_clk+1;
 						cnt_scl 	<= 4'b0;
 					end 
 				W_SLV_ADDR:
 					begin
 						sda_tri 	<= 1'b0;
-						scl_o 		<= (cnt_clk<T1) ? 1'b0 : (cnt_clk<T2 ? 1'b1 : 1'b0); 
-						sda_o 		<= Slv_Addr_i[6-cnt_scl]; 
+						scl_r 		<= (cnt_clk<T1) ? 1'b0 : (cnt_clk<T2 ? 1'b1 : 1'b0); 
+						sda_out_r 		<= Slv_Addr_i[6-cnt_scl]; 
 						cnt_clk 	<= (cnt_clk>=DIV-1) ? 0 : cnt_clk+1; 
 						cnt_scl 	<= (cnt_clk>=DIV-1) ? cnt_scl+ 1 : cnt_scl;
 					end   
 				W_WR:
 					begin
 						sda_tri 	<= 1'b0;
-						scl_o 		<= (cnt_clk<T1) ? 1'b0 : (cnt_clk<T2 ? 1'b1 : 1'b0); 
-						sda_o 		<= 1'b0;//发送写标志，低电平
+						scl_r 		<= (cnt_clk<T1) ? 1'b0 : (cnt_clk<T2 ? 1'b1 : 1'b0); 
+						sda_out_r 		<= 1'b0;//发送写标志，低电平
 						cnt_clk 	<= (cnt_clk>=DIV-1) ? 0 : cnt_clk+1;
 						cnt_scl 	<= 4'b0;
 					end 
 				W_ACK1:
 					begin
 						sda_tri 	<= 1'b1;
-						scl_o 		<= (cnt_clk<T1) ? 1'b0 : (cnt_clk<T2 ? 1'b1 : 1'b0); 
+						scl_r 		<= (cnt_clk<T1) ? 1'b0 : (cnt_clk<T2 ? 1'b1 : 1'b0); 
 						cnt_clk 	<= (cnt_clk>=DIV-1) ? 0 : cnt_clk+1;
 						cnt_scl 	<= 4'b0;
 					end      
 				W_REG_ADDR:
 					begin
 						sda_tri 	<= 1'b0;
-						scl_o 		<= (cnt_clk<T1) ? 1'b0 : (cnt_clk<T2 ? 1'b1 : 1'b0); 
-						sda_o 		<= Reg_Addr_i[7- cnt_scl];
+						scl_r 		<= (cnt_clk<T1) ? 1'b0 : (cnt_clk<T2 ? 1'b1 : 1'b0); 
+						sda_out_r 		<= Reg_Addr_i[7- cnt_scl];
 						cnt_clk 	<= (cnt_clk>=DIV-1) ? 0 : cnt_clk+1;
 						cnt_scl 	<= (cnt_clk>=DIV-1) ? cnt_scl+ 1 : cnt_scl;
 					end    
 				W_ACK2:
 					begin
 						sda_tri 	<= 1'b1;
-						scl_o 		<= (cnt_clk<T1) ? 1'b0 : (cnt_clk<T2 ? 1'b1 : 1'b0); 
+						scl_r 		<= (cnt_clk<T1) ? 1'b0 : (cnt_clk<T2 ? 1'b1 : 1'b0); 
 						cnt_clk 	<= (cnt_clk>=DIV-1) ? 0 : cnt_clk+1;
 						cnt_scl 	<= 4'b0;
 					end   
 				W_DATA:
 					begin
 						sda_tri 	<= 1'b0;
-						scl_o 		<= (cnt_clk<T1) ? 1'b0 : (cnt_clk<T2 ? 1'b1 : 1'b0); 
-						sda_o 		<= Data_i[7- cnt_scl];
+						scl_r 		<= (cnt_clk<T1) ? 1'b0 : (cnt_clk<T2 ? 1'b1 : 1'b0); 
+						sda_out_r 		<= Data_i[7- cnt_scl];
 						cnt_clk 	<= (cnt_clk>=DIV-1) ? 0 : cnt_clk+1;
 						cnt_scl 	<= (cnt_clk>=DIV-1) ? cnt_scl+ 1 : cnt_scl;
 					end     
 				W_ACK3:
 					begin
 						sda_tri 	<= 1'b1;
-						scl_o 		<= (cnt_clk<T1) ? 1'b0 : (cnt_clk<T2 ? 1'b1 : 1'b0);  
+						scl_r 		<= (cnt_clk<T1) ? 1'b0 : (cnt_clk<T2 ? 1'b1 : 1'b0);  
 						cnt_clk 	<= (cnt_clk>=DIV-1) ? 0 : cnt_clk+1;
 						cnt_scl 	<= 4'b0;
 					end       
 				STOP:
 					begin
 						sda_tri 	<= 1'b0;
-						scl_o 		<= (cnt_clk<T1) ? 1'b0 : 1'b1; 
-						sda_o 		<= (cnt_clk<T2) ? 1'b0 : 1'b1; 
+						scl_r 		<= (cnt_clk<T1) ? 1'b0 : 1'b1; 
+						sda_out_r 	<= (cnt_clk<T2) ? 1'b0 : 1'b1; 
 						cnt_clk 	<= (cnt_clk>=DIV-1) ? 0 : cnt_clk+1;
 						cnt_scl 	<= 4'b0;
 					end 
 				FINISH:
 				    begin
 						sda_tri 	<= 1'b0;
-						scl_o 		<= 1'b1;
-						sda_o 		<= 1'b1;
+						scl_r 		<= 1'b0;
+						sda_out_r 	<= 1'b0;
 						cnt_clk 	<= 0;
-						cnt_scl 	<= 0;
+						cnt_scl 	<= 4'b0;
 					end	
 				R_START2:
 					begin
 						sda_tri 	<= 1'b0;
-						scl_o 		<= (cnt_clk<T2) ? 1'b1 : 1'b0; 
-						sda_o 		<= (cnt_clk<T1) ? 1'b1 : 1'b0; 
+						scl_r 		<= (cnt_clk<T2) ? 1'b1 : 1'b0; 
+						sda_out_r 		<= (cnt_clk<T1) ? 1'b1 : 1'b0; 
 						cnt_clk 	<= (cnt_clk>=DIV-1) ? 0 : cnt_clk+1;
 						cnt_scl 	<= 4'b0;
 					end   
 				R_SLV_ADDR2:
 					begin
 						sda_tri 	<= 1'b0;
-						scl_o 		<= (cnt_clk<T1) ? 1'b0 : (cnt_clk<T2 ? 1'b1 : 1'b0); 
-						sda_o 		<= Slv_Addr_i[6-cnt_scl]; 
+						scl_r 		<= (cnt_clk<T1) ? 1'b0 : (cnt_clk<T2 ? 1'b1 : 1'b0); 
+						sda_out_r 		<= Slv_Addr_i[6-cnt_scl]; 
 						cnt_clk 	<= (cnt_clk>=DIV-1) ? 0 : cnt_clk+1; 
 						cnt_scl 	<= (cnt_clk>=DIV-1) ? cnt_scl+ 1 : cnt_scl;
 					end 
 				R_RD:
 					begin
 						sda_tri 	<= 1'b0;
-						scl_o 		<= (cnt_clk<T1) ? 1'b0 : (cnt_clk<T2 ? 1'b1 : 1'b0); 
-						sda_o 		<= 1'b1;//发送读标志
+						scl_r 		<= (cnt_clk<T1) ? 1'b0 : (cnt_clk<T2 ? 1'b1 : 1'b0); 
+						sda_out_r 		<= 1'b1;//发送读标志
 						cnt_clk 	<= (cnt_clk>=DIV-1) ? 0 : cnt_clk+1;
 						cnt_scl 	<= 4'b0;
 					end     
 				R_ACK3:
 					begin
 						sda_tri 	<= 1'b1;
-						scl_o 		<= (cnt_clk<T1) ? 1'b0 : (cnt_clk<T2 ? 1'b1 : 1'b0); 
+						scl_r 		<= (cnt_clk<T1) ? 1'b0 : (cnt_clk<T2 ? 1'b1 : 1'b0); 
 						cnt_clk 	<= (cnt_clk>=DIV-1) ? 0 : cnt_clk+1;
 						cnt_scl 	<= 4'b0;
 					end 
 				R_DATA:
 					begin
 						sda_tri 	<= 1'b1;
-						scl_o 		<= (cnt_clk<T1) ? 1'b0 : (cnt_clk<T2 ? 1'b1 : 1'b0); 
+						scl_r 		<= (cnt_clk<T1) ? 1'b0 : (cnt_clk<T2 ? 1'b1 : 1'b0); 
 						cnt_clk 	<= (cnt_clk>=DIV-1) ? 0 : cnt_clk+1;
 						cnt_scl 	<= (cnt_clk==DIV/2-1) ? cnt_scl+ 1 : cnt_scl;
 						case(cnt_scl)
-							4'b0000: IIC_Read_Data_o[7] <= sda;
-							4'b0001: IIC_Read_Data_o[6] <= sda;
-							4'b0010: IIC_Read_Data_o[5] <= sda;
-							4'b0011: IIC_Read_Data_o[4] <= sda;
-							4'b0100: IIC_Read_Data_o[3] <= sda;
-							4'b0101: IIC_Read_Data_o[2] <= sda;
-							4'b0110: IIC_Read_Data_o[1] <= sda;
-							4'b0111: IIC_Read_Data_o[0] <= sda;                                                                                                                                                                                                                                                        
+							4'b0000: IIC_Read_Data_r[7] <= oled_sda_io;
+							4'b0001: IIC_Read_Data_r[6] <= oled_sda_io;
+							4'b0010: IIC_Read_Data_r[5] <= oled_sda_io;
+							4'b0011: IIC_Read_Data_r[4] <= oled_sda_io;
+							4'b0100: IIC_Read_Data_r[3] <= oled_sda_io;
+							4'b0101: IIC_Read_Data_r[2] <= oled_sda_io;
+							4'b0110: IIC_Read_Data_r[1] <= oled_sda_io;
+							4'b0111: IIC_Read_Data_r[0] <= oled_sda_io;                                                                                                                                                                                                                                                        
                         endcase
                     end   
 				R_ACK4://只有这个是主机发送应答
 					begin
 						sda_tri 	<= 1'b0;
-						scl_o 		<= (cnt_clk<T1) ? 1'b0 : (cnt_clk<T2 ? 1'b1 : 1'b0); 
+						scl_r 		<= (cnt_clk<T1) ? 1'b0 : (cnt_clk<T2 ? 1'b1 : 1'b0); 
 						cnt_clk 	<= (cnt_clk>=DIV-1) ? 0 : cnt_clk+1;
 						cnt_scl 	<= 4'b0;
-                        sda_o<=1;//主机发送高电平无效应答
-					end                    
+                        sda_out_r<=1;//主机发送高电平无效应答
+					end    
+				default://若不跳入以上状态，则STOP
+					begin
+						sda_tri 	<= 1'b0;
+						scl_r 		<= (cnt_clk<T1) ? 1'b0 : 1'b1; 
+						sda_out_r 		<= (cnt_clk<T2) ? 1'b0 : 1'b1; 
+						cnt_clk 	<= (cnt_clk>=DIV-1) ? 0 : cnt_clk+1;
+						cnt_scl 	<= 4'b0;
+					end                 
             endcase
 end
 end
-
-//例化108MHz时钟
+    	
+wire oled_pclk_r/* synthesis mark_debug=true */;
+assign oled_pclk_o = oled_pclk_r;
+wire locked;
+//108MHz时钟
+  clk_wiz_0 instance_name
+   (
+    // Clock out ports
+    .clk_out1(oled_pclk_r),     // output clk_out1
+    // Status and control signals
+    .resetn(rst_n), // input resetn
+    .locked(locked),       // output locked
+   // Clock in ports
+    .clk_in1(cmos_data_clk_150m_o));      // input clk_in1
 
 endmodule
